4f6c4cea9d421fc2779305e2565786febe9bcd90
katernst <katernst@umich.edu>
2015-06-08 23:28:12 +0200

    something stupid

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'users/index'
 
-  resources :restaurants
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
