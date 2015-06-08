ddc054033ef779f34715e4c73491c5488dbb94ce
katernst <katernst@umich.edu>
2015-06-09 00:19:34 +0200

    registrations controller and bootstrap installation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :shelters
 
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   get 'users/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
