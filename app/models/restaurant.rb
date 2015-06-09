0fd14aedec426782437c75c6c26d1150475b9989
pvselvan <pvselvan@umich.edu>
2015-06-09 15:25:15 +0200

    mapped restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ActiveRecord::Base
+
+	geocoded_by :address
+	after_validation :geocode, :if => :address_changed?
+	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608193707) do
+ActiveRecord::Schema.define(version: 20150609091903) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "shelters", force: :cascade do |t|
